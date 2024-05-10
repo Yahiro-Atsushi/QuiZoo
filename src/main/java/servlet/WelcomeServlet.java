cee0b5b0e054c365997474a9ab7f4665d1679f02
八尋 温 <xxx>
2024-05-10 13:10:07 +0900

    index.jspを表示するWelcomeServlet.javaを作成

---
+++ src/main/java/servlet/WelcomeServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Address;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		RequestDispatcher rdp = request.getRequestDispatcher(Address.INDEX.getAddress());
+		rdp.forward(request, response);
+		
+	}
+
+
+}
