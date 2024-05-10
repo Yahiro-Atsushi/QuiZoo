518b9e09902cf9a9da1aa2a0364820522f9906a5
八尋 温 <xxx>
2024-05-10 17:01:57 +0900

    衝突回避

--- src/main/java/model/GameMode.java
+++ src/main/java/model/GameMode.java
@@ -1,17 +1,24 @@
 package model;
 
 public enum GameMode {
-	EASY("チンパンジー"), 
-	NORMAL("人間"), 
-	HARD("オランウータン");
+	EASY("チンパンジー", " quiz_easy "), 
+	NORMAL("人間", " quiz_normal "), 
+	HARD("オランウータン", " quiz_hard "),
+	CHALLENGE("キメラ", "quiz_challenge");
 
-	private String mode;
+	private String animal;
+	private String table;
 
-	private GameMode(String mode) {
-		this.mode = mode;
+	private GameMode(String animal, String table) {
+		this.animal = animal;
+		this.table = table;
 	}
 
 	public String getValue() {
-		return this.mode;
+		return this.animal;
+	}
+	
+	public String getTable() {
+		return this.table;
 	}
 }
