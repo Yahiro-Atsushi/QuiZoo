b22eb187a60da88a63a10a90a02e54b7e5ee47a1
高山 那美 <abc>
2024-05-14 15:15:32 +0900

    LoginDAOTestの作成

---
+++ src/main/java/model/test/LoginDAOTest.java
@@ -0,0 +1,38 @@
+package model.test;
+
+import model.User;
+import model.logic.LoginLogic;
+
+public class LoginDAOTest {
+
+	public static void main(String[] args) {
+		testExecuteOK(); // ログイン成功のテスト
+		testExecuteNG(); // ログイン失敗のテスト
+	}
+
+	public static void testExecuteOK() {
+		User use = new User("minato", "1234");
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(use);
+		if (result) {
+			System.out.println("testExecuteOK：成功しました");
+		} else {
+			System.out.println("testExecuteOK：失敗しました");
+		}
+	}
+
+	public static void testExecuteNG() {
+		User user = new User("minato", "12345");
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(user);
+		if (!result) {
+			System.out.println("testExecuteNG：成功しました");
+		} else {
+			System.out.println("testExecuteNG：失敗しました");
+		}
+
+
+	}
+	
+
+}
