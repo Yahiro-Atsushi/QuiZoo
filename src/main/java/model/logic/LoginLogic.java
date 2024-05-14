18e37c5d266caeb98639eac3b9a1374d04267af0
高山 那美 <abc>
2024-05-14 15:13:21 +0900

    LoginLogicの作成

---
+++ src/main/java/model/logic/LoginLogic.java
@@ -0,0 +1,14 @@
+package model.logic;
+
+import database.AccountsDAO;
+import database.AccountsDTO;
+import model.User;
+
+public class LoginLogic {
+	public boolean execute(User user) {
+		AccountsDAO accountDAO = new AccountsDAO();
+		AccountsDTO account = accountDAO.findByLogin(user);
+		return account != null;
+	}
+
+}
