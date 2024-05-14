b848c93bd783aee7830b040dad125b6843259a78
高山 那美 <abc>
2024-05-14 15:17:46 +0900

    LoginSevlet・RegisterServlet・register.jsp の修正・追加

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -2,35 +2,51 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Address;
 import model.User;
-
+import model.logic.LoginLogic;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
-		
+
 		User user = new User(name, pass);
-		
-		
-		
-		
+
+		// ログイン処理
+		LoginLogic loginLogic = new LoginLogic();
+		boolean isLogin = loginLogic.execute(user);
+
+		if (isLogin) {
+			ServletContext application = this.getServletContext();
+			application.setAttribute("name", name);
+
+			// ログイン成功時、main画面にフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher(Address.MAIN.name());
+			dispatcher.forward(request, response);
+
+		} else {
+			response.sendRedirect("LoginServlet");
+
+		}
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/register.jsp
+++ src/main/webapp/WEB-INF/jsp/register.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,14 +14,8 @@
 	<p  style="text-align:center">
 		名前<input type="text" name="name"><br>
 		pass<input type="password" name="pass"><br><br>
-		<input type="submit" value="ログイン">
+		<input type="submit" value="登録・ログイン">
 	</p>
 	</form>
-	<form action=" RegisterServlet">
-	<p  style="text-align:center">	
-		<input type="submit" value="新規登録">
-	</form>
-	</p>
-
 </body>
 </html>
