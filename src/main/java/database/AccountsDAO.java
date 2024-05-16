030c9f94e39317fc472c15801cb0e06a378a096e
八尋 温 <xxx>
2024-05-16 13:36:25 +0900

    入力された文字列と一致するユーザー名を探すメソッドを実装

--- src/main/java/database/AccountsDAO.java
+++ src/main/java/database/AccountsDAO.java
@@ -21,53 +21,80 @@ public class AccountsDAO {
 	public void dbClose() {
 		connector.dbClose();
 	}
-	
+
 	public User findAccountFromInput(String inputName, String inputPass) {
 		User user = null;
-		
+
 		String sql = "SELECT name, pass " +
-					"FROM accounts " +
-					"WHERE " +
-					"name = ? AND pass = ? ";
-		
-		try(PreparedStatement ps = con.prepareStatement(sql);){
-			
+				"FROM accounts " +
+				"WHERE " +
+				"name = ? AND pass = ? ";
+
+		try (PreparedStatement ps = con.prepareStatement(sql);) {
+
 			ps.setString(1, inputName);
 			ps.setString(2, inputPass);
-			
+
 			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String name = rs.getString("name");
 				String pass = rs.getString("pass");
 				user = new User(name, pass);
-				
+
 			}
 		} catch (SQLException e) {
 			System.out.println("AccountsDao.findAccountFromInput（String, String）: SELECT文実行エラー");
 			e.printStackTrace();
 		}
-		return user;	
+		return user;
 	}
-	
+
 	public void userInsert(String inputName, String inputPass) {
-		
+
 		String query = "INSERT INTO " +
-					"accounts(name, pass) " +
-					"VALUES (?, ?); ";
-		try(PreparedStatement ps = con.prepareStatement(query);) {
-			
+				"accounts(name, pass) " +
+				"VALUES (?, ?); ";
+		try (PreparedStatement ps = con.prepareStatement(query);) {
+
 			ps.setString(1, inputName);
 			ps.setString(2, inputPass);
 			ps.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			System.out.println("AccountsDao.userInsert（String, String）：INSERT文実行エラー");
 			e.printStackTrace();
 		}
-		
-		return ;
-		
-		
+
+		return;
+
+	}
+
+	public String findUserName(String inputName) {
+		String userName = null;
+
+		String query = ""
+				+ "SELECT "
+				+ " name "
+				+ "FROM "
+				+ " accounts "
+				+ "WHERE "
+				+ " name = ? ";
+
+		try (PreparedStatement ps = con.prepareStatement(query);) {
+
+			ps.setString(1, inputName);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				userName = rs.getString(ColumnNames.name.name());
+
+			}
+		} catch (SQLException e) {
+			System.out.println("AccountsDao.findUserName（String）: SELECT文実行エラー");
+			e.printStackTrace();
+		}
+		return userName;
 	}
 }
