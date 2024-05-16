fb6f134c7e0a60275a94a2affc717e4429e4e54b
八尋 温 <xxx>
2024-05-16 15:16:10 +0900

    メインサーブレット実装

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -14,9 +14,9 @@ import entity.Address;
 @WebServlet("/MainServlet")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher(Address.MAIN.getAddress());
 		dispatcher.forward(request, response);
 	}
