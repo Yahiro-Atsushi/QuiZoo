dd859d6c7d4649f4bcf95af9696b62b854efa6b1
高山 那美 <abc>
2024-05-17 15:04:15 +0900

    databaseフォルダーのAccountDAO,ColumnNames,SelectDaoにコメント追加

--- src/main/java/database/AccountsDAO.java
+++ src/main/java/database/AccountsDAO.java
@@ -22,24 +22,28 @@ public class AccountsDAO {
 		connector.dbClose();
 	}
 
+	// データベースから名前・パスワードを検索
 	public User findAccountFromInput(String inputName, String inputPass) {
 		User user = null;
 
+	// SELECT文を実行
 		String sql = "SELECT name, pass " +
 				"FROM accounts " +
 				"WHERE " +
 				"name = ? AND pass = ? ";
 
 		try (PreparedStatement ps = con.prepareStatement(sql);) {
-
+		    //name・passを取得
 			ps.setString(1, inputName);
 			ps.setString(2, inputPass);
 
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
+			// 取得した値を格納
 				String name = rs.getString("name");
 				String pass = rs.getString("pass");
+			// name・passをUserに格納
 				user = new User(name, pass);
 
 			}
@@ -47,16 +51,19 @@ public class AccountsDAO {
 			System.out.println("AccountsDao.findAccountFromInput（String, String）: SELECT文実行エラー");
 			e.printStackTrace();
 		}
+	// userを返す
 		return user;
 	}
 
+	// ユーザーを追加する
 	public void userInsert(String inputName, String inputPass) {
-
+		// INSERT文を実行
 		String query = "INSERT INTO " +
 				"accounts(name, pass) " +
 				"VALUES (?, ?); ";
+		
 		try (PreparedStatement ps = con.prepareStatement(query);) {
-
+			 //name・passに値を指定 
 			ps.setString(1, inputName);
 			ps.setString(2, inputPass);
 			ps.executeUpdate();
@@ -70,9 +77,10 @@ public class AccountsDAO {
 
 	}
 
+	// ユーザー名を検索する
 	public String findUserName(String inputName) {
 		String userName = null;
-
+		// SELECT文を実行
 		String query = ""
 				+ "SELECT "
 				+ " name "
@@ -82,12 +90,13 @@ public class AccountsDAO {
 				+ " name = ? ";
 
 		try (PreparedStatement ps = con.prepareStatement(query);) {
-
+			// nameに値を指定
 			ps.setString(1, inputName);
 
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
+			// userNameに取得した値を格納
 				userName = rs.getString(ColumnNames.name.name());
 
 			}
@@ -95,6 +104,7 @@ public class AccountsDAO {
 			System.out.println("AccountsDao.findUserName（String）: SELECT文実行エラー");
 			e.printStackTrace();
 		}
+		// userNameを返す
 		return userName;
 	}
 }
