3043b0d54720ece0e77c4dcd9382301670bc25c5
高山 那美 <abc>
2024-05-17 17:44:07 +0900

    コメントの追加 businessObjectフォルダー：ChoiceButtonTextLogic,LoginLogic,RegisterLogic,ResultLogic

--- src/main/java/businessObject/LoginLogic.java
+++ src/main/java/businessObject/LoginLogic.java
@@ -2,13 +2,18 @@ package businessObject;
 
 import database.AccountsDAO;
 import entity.User;
-
+ // ログインに関する処理をするクラス
 public class LoginLogic {
 	
 	public static User execute(String inputName, String inputPass) {
+		// データベースのAccountsテーブルの処理をまとめたクラス
+		// のインスタンスを生成
 		AccountsDAO aDao = new AccountsDAO();
+		
+		// 名前・パスワードを検索しUserに代入
 		User user = aDao.findAccountFromInput(inputName, inputPass);
 		
+		// userを返す
 		return user;
 	}
 	
--- src/main/java/businessObject/RegisterLogic.java
+++ src/main/java/businessObject/RegisterLogic.java
@@ -3,11 +3,15 @@ package businessObject;
 import database.AccountsDAO;
 import entity.User;
 
+ // ユーザー登録に関する処理を行うクラス
 public class RegisterLogic {
 	
 	public static User execute(String inputName, String inputPass) {
-		
+		// データベースのAccountsテーブルの処理をまとめたクラス
+		// のインスタンスを生成
 		AccountsDAO aDao = new AccountsDAO();
+		
+		// 入力されたユーザー名を検索しuserNameに代入する
 		String userName = aDao.findUserName(inputName);
 		
 		//すでにログインしている場合はエラーのためnullを返す
@@ -16,7 +20,9 @@ public class RegisterLogic {
 		}
 		
 		//アカウント登録したらユーザーアカウントを返す
+		// 入力されたユーザー名・パスワードをAccountsテーブルに追加する
 		aDao.userInsert(inputName, inputPass);
+		// 入力されたユーザー名・パスワードをUserに代入
 		User user = aDao.findAccountFromInput(inputName, inputPass);
 		
 		return user;
