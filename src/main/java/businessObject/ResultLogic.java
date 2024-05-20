d2c61dd0ed4b197e119528d9d6ec63ddc3793a95
八尋 温 <xxx>
2024-05-20 13:20:11 +0900

    問題数を取得し、その問題数までのMapを作成するように修正

--- src/main/java/businessObject/ResultLogic.java
+++ src/main/java/businessObject/ResultLogic.java
@@ -7,11 +7,14 @@ import entity.Game;
 
 public class ResultLogic {
 	public static Map<Integer, String>  execute(Game game) {
+		//問題数を取得
+		int quizCount = game.getQuizzes().size();
+		
 		Map<Boolean, String> correct = new HashMap<>();
 		correct.put(true, "正解");
 		correct.put(false, "不正解");
 		Map<Integer, String> result = new HashMap<>();
-		for (int i = 1; i < 11; i++) {
+		for (int i = 1; i <= quizCount; i++) {
 			result.put(i, correct.get(game.getIsCorrects().get(i)));
 		}
 		return result;
