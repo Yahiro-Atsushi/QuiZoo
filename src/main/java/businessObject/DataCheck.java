7a6742260d0c85cdf06375a7c3e24bc622f49e6f
八尋 温 <xxx>
2024-05-28 11:11:25 +0900

    パスワードの正規表現チェックLogicを実装

--- src/main/java/businessObject/DataCheck.java
+++ src/main/java/businessObject/DataCheck.java
@@ -5,9 +5,38 @@ public class DataCheck{
 	public static boolean isInputName(String data) {
 		if(isInput(data)) {
 			data.trim();
-			
 			return true;
 		}
+		
+		return false;
+	}
+	
+	public static boolean isValidPass(String data) {
+		if(!isInput(data)) {
+			return false;
+		}
+		data.trim();
+		
+		int length = data.length();
+		final int MIN = 4;
+		final int MAX = 12;
+		
+		if(!(MIN <= length && length <= MAX)) {
+			return false;
+		}
+		
+		for (char c : data.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+		
+		return true;
+	}
+
+	private static boolean isMatchedExpression(String data) {
+		String expression = "[]{4,8}";
+		
 		
 		return false;
 	}
