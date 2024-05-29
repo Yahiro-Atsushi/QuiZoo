6083a66b9b871c552ca9be0d6345f1f1f58b5da6
八尋 温 <xxx>
2024-05-29 10:43:11 +0900

    整形

--- src/main/java/businessObject/DataCheck.java
+++ src/main/java/businessObject/DataCheck.java
@@ -1,55 +1,55 @@
 package businessObject;
 
-public class DataCheck{
+public class DataCheck {
 
 	public static boolean isInputName(String data) {
-		if(!isInput(data)) {
+		if (!isInput(data)) {
 			return false;
 		}
-		
+
 		int length = data.length();
 		final int MIN = 1;
 		final int MAX = 12;
-		
-		if(!(MIN <= length && length <= MAX)) {
+
+		if (!(MIN <= length && length <= MAX)) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	public static boolean isValidPass(String data) {
-		if(!isInput(data)) {
+		if (!isInput(data)) {
 			return false;
 		}
-		
+
 		int length = data.length();
 		final int MIN = 4;
 		final int MAX = 12;
-		
-		if(!(MIN <= length && length <= MAX)) {
+
+		if (!(MIN <= length && length <= MAX)) {
 			return false;
 		}
-		
-		if(!isAlphanumericChars(data)){
+
+		if (!isAlphanumericChars(data)) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
 	private static boolean isAlphanumericChars(String data) {
 		for (char c : data.toCharArray()) {
-            if (!Character.isLetterOrDigit(c)) {
-                return false;
-            }
-        }
-		
+			if (!Character.isLetterOrDigit(c)) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 
 	private static boolean isInput(String data) {
-		if(data != null&& data.length() != 0) {
+		if (data != null && data.length() != 0) {
 			return true;
 		}
 		return false;
